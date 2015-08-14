514a12ea99f6a39243390f6ecd7004aad6bfb280
aciurea <alinciurea90@gmail.com>
2015-08-14 11:57:23 +0300

    Created the Project. Start with Model, View Controller course

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTraining.Startup))]
+namespace MVCTraining
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
